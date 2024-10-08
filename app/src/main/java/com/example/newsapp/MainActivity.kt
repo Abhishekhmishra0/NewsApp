3bae89a33c1ec573127e3feaa94cb8365327c02f
abhishekhmishra <mabhishekh51@gmail.com>
2024-10-08 23:23:35 +0530

    initial commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModelProvider
+import com.example.newsapp.ui.theme.HomePage
+import com.example.newsapp.ui.theme.NewsAppTheme
+import com.example.newsapp.ui.theme.NewsViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val newsViewModel = ViewModelProvider(this)[NewsViewModel::class.java]
+        setContent {
+            NewsAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier.padding()
+                    ) {
+                        Text(text = "NEWS NOW",
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                            color = Color.Red,
+                            fontSize = 25.sp,
+                            fontFamily = FontFamily.Serif
+                        )
+                            HomePage(newsViewModel)
+                    }
+                }
+            }
+        }
+    }
+}
+
